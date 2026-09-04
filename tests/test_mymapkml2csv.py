0ebd33b384ffd8ab5f6e12b53f58190e99a2ceb6
hilary <hilarysheen.s@gmail.com>
2026-09-04 14:34:09 +0800

    add kml fixtures and tests for extract_polygons function

--- tests/test_mymapkml2csv.py
+++ tests/test_mymapkml2csv.py
@@ -26,69 +26,101 @@ def polygon_coords():
 
 
 @pytest.fixture
-def polygon_placemarks():
-    xml_text = """
-    <Placemark>
-      <name>polygon01</name>
-      <description>1_Grona_triflora</description>
-      <styleUrl>#poly-E65100-1200-77</styleUrl>
-      <Polygon>
-        <outerBoundaryIs>
-          <LinearRing>
-            <tessellate>1</tessellate>
-            <coordinates>
-              121.6028361,25.082438,0
-              121.6027623,25.082449,0
-              121.6027368,25.0823895,0
-              121.6027703,25.082353,0
-              121.602844,25.082375,0
-              121.6028361,25.082438,0
-            </coordinates>
-          </LinearRing>
-        </outerBoundaryIs>
-      </Polygon>
-    </Placemark>
-    <Placemark>
-      <name>polygon02</name>
-      <description>X</description>
-      <styleUrl>#poly-000000-1200-77</styleUrl>
-      <Polygon>
-        <outerBoundaryIs>
-          <LinearRing>
-            <tessellate>1</tessellate>
-            <coordinates>
-              121.6025583,25.0823609,0
-              121.6025583,25.082446,0
-              121.6024778,25.0824229,0
-              121.6024751,25.0823512,0
-              121.6025583,25.0823609,0
-            </coordinates>
-          </LinearRing>
-        </outerBoundaryIs>
-      </Polygon>
-    </Placemark>
-    <Placemark>
-      <name>polygon03</name>
-      <description>1_Grona_triflora, 2_Grona_heterophylla</description>
-      <styleUrl>#poly-E65100-1200-77</styleUrl>
-      <Polygon>
-        <outerBoundaryIs>
-          <LinearRing>
-            <tessellate>1</tessellate>
-            <coordinates>
-              121.6025381,25.0824897,0
-              121.6025207,25.0825383,0
-              121.6024282,25.0825006,0
-              121.6024671,25.0824472,0
-              121.6025381,25.0824897,0
-            </coordinates>
-          </LinearRing>
-        </outerBoundaryIs>
-      </Polygon>
-    </Placemark>
-    """
+def point_placemark_file(tmp_path):
+    fp = tmp_path / "point_placemark.kml"
 
-    return ET.fromstring(xml_text)
+    xml_text = """<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+      <Document>
+        <Placemark>
+          <name>0621</name>
+          <description>8_Arachis_pintoi</description>
+          <styleUrl>#icon-1899-0288D1</styleUrl>
+          <Point>
+            <coordinates>
+              121.59205,25.043945,0
+            </coordinates>
+          </Point>
+        </Placemark>
+      </Document>
+    </kml>
+    """
+    fp.write_text(xml_text, encoding="utf-8")
+
+    return fp
+
+
+@pytest.fixture
+def polygon_placemarks_file(tmp_path):
+    fp = tmp_path / "polygon_placemarks.kml"
+
+    xml_text = """<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+      <Document>
+          <Placemark>
+          <name>polygon01</name>
+          <description>1_Grona_triflora</description>
+          <styleUrl>#poly-E65100-1200-77</styleUrl>
+          <Polygon>
+            <outerBoundaryIs>
+              <LinearRing>
+                <tessellate>1</tessellate>
+                <coordinates>
+                  121.6028361,25.082438,0
+                  121.6027623,25.082449,0
+                  121.6027368,25.0823895,0
+                  121.6027703,25.082353,0
+                  121.602844,25.082375,0
+                  121.6028361,25.082438,0
+                </coordinates>
+              </LinearRing>
+            </outerBoundaryIs>
+          </Polygon>
+        </Placemark>
+        <Placemark>
+          <name>polygon02</name>
+          <description>X</description>
+          <styleUrl>#poly-000000-1200-77</styleUrl>
+          <Polygon>
+            <outerBoundaryIs>
+              <LinearRing>
+                <tessellate>1</tessellate>
+                <coordinates>
+                  121.6025583,25.0823609,0
+                  121.6025583,25.082446,0
+                  121.6024778,25.0824229,0
+                  121.6024751,25.0823512,0
+                  121.6025583,25.0823609,0
+                </coordinates>
+              </LinearRing>
+            </outerBoundaryIs>
+          </Polygon>
+        </Placemark>
+        <Placemark>
+          <name>polygon03</name>
+          <description>1_Grona_triflora, 2_Grona_heterophylla</description>
+          <styleUrl>#poly-E65100-1200-77</styleUrl>
+          <Polygon>
+            <outerBoundaryIs>
+              <LinearRing>
+                <tessellate>1</tessellate>
+                <coordinates>
+                  121.6025381,25.0824897,0
+                  121.6025207,25.0825383,0
+                  121.6024282,25.0825006,0
+                  121.6024671,25.0824472,0
+                  121.6025381,25.0824897,0
+                </coordinates>
+              </LinearRing>
+            </outerBoundaryIs>
+          </Polygon>
+        </Placemark>
+      </Document>
+    </kml>
+    """
+    fp.write_text(xml_text, encoding="utf-8")
+
+    return fp
 
 
 @pytest.fixture
@@ -140,3 +172,34 @@ def test_polygon_centroid_empty():
 
     assert lat is None
     assert lon is None
+
+
+def test_extract_empty_polygons(point_placemark_file):
+    result = kml2csv.extract_polygons(point_placemark_file)
+
+    assert result == []
+
+
+def test_extract_polygons(polygon_placemarks_file):
+    result = kml2csv.extract_polygons(polygon_placemarks_file)
+
+    assert result == [
+        {
+            "survey_site": "polygon01",
+            "latitude": 25.082407083333333,
+            "longitude": 121.60279759999999,
+            "description": "1_Grona_triflora",
+        },
+        {
+            "survey_site": "polygon02",
+            "latitude": 25.08238838,
+            "longitude": 121.60252556,
+            "description": "X",
+        },
+        {
+            "survey_site": "polygon03",
+            "latitude": 25.0824931,
+            "longitude": 121.60249844,
+            "description": "1_Grona_triflora, 2_Grona_heterophylla",
+        },
+    ]
